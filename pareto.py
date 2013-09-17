c7cbc4a10ee2ec0e0a92bfd7764ad56fff08c1e2
Matthew Woodruff <mjw5407@psu.edu>
2013-09-17 15:20:20 -0400

    Added get_args

--- pareto.py
+++ pareto.py
@@ -34,7 +34,9 @@ For epsilon-nondomination:
 @article{deb_2005_emoea,
     author = { Deb, K. and Mohan, M. and Mishra, S},
     year = {2005},
-    title = {Evaluating the $\\varepsilon$-domination based multiobjective evolutionary algorithm for a quick computation of Pareto-optimal solutions.},
+    title = {Evaluating the $\\varepsilon$-domination based
+        multiobjective evolutionary algorithm for a quick
+        computation of Pareto-optimal solutions.},
     journal = {Evolutionary Computation Journal},
     volume= {13}, number = {4}, pages ={501--525}
 }
@@ -138,49 +140,62 @@ class Archive(object):
         # if you get here, then no archive solution has dominated this one
         self.add(solution, sobj, sbox)
 
-# Get command line arguments and check for errors
-parser = argparse.ArgumentParser(description='Nondomination Sort for Multiple Files')
-parser.add_argument('--objectives', type=int, nargs='+', required=False, help='Objective Columns (zero-indexed)')
-parser.add_argument('--epsilons', type=float, nargs='+', required=False, help='Epsilons, one per objective')
-parser.add_argument('-o', '--output', type=str, required=True, help='Output Filename')
-parser.add_argument('-i', '--input', type=str, required=True, nargs='+', help='Input filenames')
-parser.add_argument('--delimiter', type=str, required=False, default=' ', help='Input column delimiter')
-parser.add_argument('--print-only-objectives', action='store_true', default=False, required=False, help='Print only objectives in output')
-parser.add_argument('--precision', type=int, required=False, default=8, help='Output floating-point precision')
-args = parser.parse_args()
+def get_args():
+    # Get command line arguments and check for errors
+    parser = argparse.ArgumentParser(
+        description='Nondomination Sort for Multiple Files')
+    parser.add_argument('--objectives', type=int, nargs='+', required=False,
+                        help='Objective Columns (zero-indexed)')
+    parser.add_argument('--epsilons', type=float, nargs='+', required=False,
+                        help='Epsilons, one per objective')
+    parser.add_argument('-o', '--output', type=str,
+                        required=True, help='Output Filename')
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        nargs='+', help='Input filenames')
+    parser.add_argument('--delimiter', type=str, required=False,
+                        default=' ', help='Input column delimiter')
+    parser.add_argument('--print-only-objectives', action='store_true',
+                        default=False, required=False,
+                        help='Print only objectives in output')
+    parser.add_argument('--precision', type=int, required=False, default=8,
+                        help='Output floating-point precision')
+    return parser.parse_args()
 
-# Get the first input file to check size
-tempinput = np.loadtxt(args.input[0], delimiter=args.delimiter)
+if __name__ == "__main__":
+    args = get_args()
 
-# If objectives not given, use all columns
-if not args.objectives:
-  args.objectives = range(tempinput[0,:].size)
-# If given objectives are out of bounds, exit with error
-elif (min(args.objectives) < 0 or max(args.objectives) > tempinput[0,:].size):
-  msg = "Error: One or more objective values exceed input matrix bounds\n"
-  sys.stdout.write(msg)
-  exit()
-if args.epsilons:
-  if len(args.epsilons) != len(args.objectives): # make sure number of epsilons matches number of objectives
-    msg =  "Error: Number of epsilon values must match number of objectives.\n"
-    sys.stdout.write(msg)
-    exit()
-else:
-  args.epsilons = [1e-9]*len(args.objectives) # if epsilons not defined, use 1e-9 for all objectives
+    # Get the first input file to check size
+    tempinput = np.loadtxt(args.input[0], delimiter=args.delimiter)
 
-archive = Archive(args.epsilons, args.objectives)
+    # If objectives not given, use all columns
+    if not args.objectives:
+      args.objectives = range(tempinput[0,:].size)
+    # If given objectives are out of bounds, exit with error
+    elif (min(args.objectives) < 0 or max(args.objectives) > tempinput[0,:].size):
+      msg = "Error: One or more objective values exceed input matrix bounds\n"
+      sys.stdout.write(msg)
+      exit()
+    if args.epsilons:
+      if len(args.epsilons) != len(args.objectives): # make sure number of epsilons matches number of objectives
+        msg =  "Error: Number of epsilon values must match number of objectives.\n"
+        sys.stdout.write(msg)
+        exit()
+    else:
+      args.epsilons = [1e-9]*len(args.objectives) # if epsilons not defined, use 1e-9 for all objectives
 
-# for each file in argument list ...
-for filename in args.input:
-  solutions = np.loadtxt(filename, delimiter=args.delimiter)
+    archive = Archive(args.epsilons, args.objectives)
 
-  # for each line in file (new candidate solution) ...
-  for i in xrange(0, solutions[:,0].size):
-    candidateSolution = solutions[i,:]
-    archive.sortinto(candidateSolution)
+    # for each file in argument list ...
+    for filename in args.input:
+      solutions = np.loadtxt(filename, delimiter=args.delimiter)
 
-# Convert ParetoSet to nparray and print it
-ParetoSet = np.array(archive.archive)
-if args.print_only_objectives:
-  ParetoSet = ParetoSet[:, args.objectives]
-np.savetxt(args.output, ParetoSet, delimiter=args.delimiter, fmt='%.' + str(args.precision) + 'e')
+      # for each line in file (new candidate solution) ...
+      for i in xrange(0, solutions[:,0].size):
+        candidateSolution = solutions[i,:]
+        archive.sortinto(candidateSolution)
+
+    # Convert ParetoSet to nparray and print it
+    ParetoSet = np.array(archive.archive)
+    if args.print_only_objectives:
+      ParetoSet = ParetoSet[:, args.objectives]
+    np.savetxt(args.output, ParetoSet, delimiter=args.delimiter, fmt='%.' + str(args.precision) + 'e')
